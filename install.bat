6894b1dee662ecbbc82c6dfc63d7e3648a15c4fb
algoExpert <ghaithsassi@github.com>
2020-01-10 16:29:52 -0800

    add analyzer linker

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@ g++ -c stream.cpp
 g++ -c file.cpp 
 g++ -c index.cpp 
 g++ -c ranking.cpp
+g++ -c analyzer.cpp
 g++ -c engine.cpp -fopenmp
 g++ -c main.cpp -fopenmp
-g++ word.o stream.o file.o index.o ranking.o engine.o main.o -fopenmp -o f
+g++ word.o stream.o file.o index.o ranking.o analyzer.o engine.o main.o -fopenmp -o f
